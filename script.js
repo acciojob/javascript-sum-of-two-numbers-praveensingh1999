e0eac7a59b38b32438014745c36f3d853f068472
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-20 22:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let input1 = prompt("Enter the first number:");
-let input2 = prompt("Enter the second number:");
+ let input1 = prompt("Enter the first number:");
+ let input2 = prompt("Enter the second number:"); 
+let flag = false;
+if(input1 == "" || input2 == ""){ 
+	flag=true;
+}
+input1 = Number(input1);
+input2 = Number(input2);
+if(isNaN(input1) == false && isNaN(input2) == false && flag == false){
+	alert(`The sum of ${input1} and ${input2} is ${Number(input1) + Number(input2)}.`);
+}
+else{     
+	alert(`Invalid input. Please enter a valid number.`)
+}
 
 // Write your code here and print the output using alert function
